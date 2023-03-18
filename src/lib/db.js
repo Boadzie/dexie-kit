3688bda6b8b6a978b5cc08dcae9a6cc05d4663f9
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-03-18 12:31:31 +0000

    Add a Dexie.js store called booksDB
    - Add a table caled books
    - Add `++id, title, genre and price` fields to the table
    - Add dummyData to prepopulate the store

---
+++ src/lib/db.js
@@ -0,0 +1,52 @@
+// db.js
+import Dexie from 'dexie';
+
+export const db = new Dexie('bookDB');
+db.version(1).stores({
+	books: '++id, title, author, genre, price' // Primary key and indexed props
+});
+
+const dummyData = [
+	{
+		title: 'The Great Gatsby',
+		author: 'F. Scott Fitzgerald',
+		genre: 'Fiction',
+		price: 12.99
+	},
+	{
+		title: 'To Kill a Mockingbird',
+		author: 'Harper Lee',
+		genre: 'Fiction',
+		price: 10.99
+	},
+	{
+		title: 'The Catcher in the Rye',
+		author: 'J.D. Salinger',
+		genre: 'Fiction',
+		price: 9.99
+	},
+	{
+		title: 'The Hobbit',
+		author: 'J.R.R. Tolkien',
+		genre: 'Fantasy',
+		price: 14.99
+	},
+	{
+		title: 'The Lord of the Rings',
+		author: 'J.R.R. Tolkien',
+		genre: 'Fantasy',
+		price: 24.99
+	},
+	{
+		title: 'The Da Vinci Code',
+		author: 'Dan Brown',
+		genre: 'Thriller',
+		price: 7.99
+	}
+];
+
+db.on('populate', function (transaction) {
+	transaction.books.bulkAdd(dummyData);
+});
+
+db.open();
