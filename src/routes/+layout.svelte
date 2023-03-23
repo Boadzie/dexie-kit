e0999440427c9100a8d5b96151b09a9b53c49632
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-03-23 08:54:56 +0000

    Add new font from Google
    Set the font globally in the layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="flex bg-slate-800 text-white p-3 gap-4 justify-between items-center">
+	<a href="/" class="px-2 text-3xl ">Dexie-Kit</a>
+	<a class="text-2xl px-2  rounded-sm hover:text-green-500" href="#addBook">Add Book</a>
+</div>
+<main><slot /></main>
+
+<style>
+	:global(html) {
+		font-family: 'Titillium Web', sans-serif;
+	}
+</style>
